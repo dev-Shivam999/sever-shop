f979a5e33dfe9f26d059bdddda866d23a9d54126
dev-Shivam999 <shivamswami982@gmail.com>
2024-04-16 16:52:00 +0530

    last

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import { router } from './router/user.js';
 export const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:['http://localhost:5173','*'],
+    origin:['http://localhost:5173','https://harshu-shop.netlify.app'],
     credentials:true,
 }))
 
